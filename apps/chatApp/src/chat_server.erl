7ab5aaecab63fe7cae2eded8e60331d4577c03a4
Pratima Chainani <pratimac@thoughtworks.com>
2018-09-12 14:38:34 +0530

    [PC] - Use async genserver calls for pub/sub

--- apps/chatApp/src/chat_server.erl
+++ apps/chatApp/src/chat_server.erl
@@ -3,17 +3,16 @@
 -behaviour(gen_server).
 
 -define(SERVER, ?MODULE).
--define(CLIENT_ID, "client_id").
+-define(CLIENT_ID, "<<pc_client_id>>").
 
 -export([start/2, stop/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2]).
--export([subscribe/3, publish/4]).
+-export([subscribe/2, publish/3]).
 
 -record(state, {
   recipient :: pid(),
   host = "localhost" :: inet:ip_address() | string(),
-  port = 1883 :: inet:port_number(),
-  seq
+  port = 1883 :: inet:port_number()
 }).
 
 start(_StartType, _StartArgs) ->
@@ -28,47 +27,33 @@ init(_Arg0) ->
   {ok, C} = emqttc:start_link([
     {host, "localhost"},
     {port, 1883},
-    {client_id, ?CLIENT_ID},
+    {client_id, <<"pc_client_id">>},
     {username, <<"pratima">>},
     {password, <<"password">>}]),
 
-  %% The pending subscribe
-%%  emqttc:subscribe(C, <<"TopicA">>, 1),
-  {ok, #state{recipient = C, seq = 1}}.
+  {ok, #state{recipient = C}}.
 
 handle_call(stop, _From, State) ->
-  {stop, normal, ok, State};
+  {stop, normal, ok, State}.
 
-handle_call({subscribe, Topic, QoS}, _From, State = #state{recipient = C}) ->
+handle_cast({subscribe, Topic, QoS}, State = #state{recipient = C}) ->
   emqttc:subscribe(C, list_to_binary([Topic]), QoS),
-  {reply, ok, State};
+  {noreply, State};
 
-handle_call({publish, Topic, Message, QoS}, _From, State = #state{recipient = C}) ->
+handle_cast({publish, Topic, Message, QoS}, State = #state{recipient = C}) ->
   emqttc:publish(C, list_to_binary([Topic]), list_to_binary([Message]), QoS),
-  {reply, ok, State}.
-
-handle_cast(_Msg, State) ->
   {noreply, State}.
 
 handle_info({mqttc, C, connected}, State = #state{recipient = C}) ->
   io:format("Client ~p is connected~n", [C]),
   {noreply, State};
 
-handle_info({subscribe, Topic, QoS}, State = #state{recipient = C}) ->
-  emqttc:subscribe(C, list_to_binary([Topic]), QoS),
-  {noreply, State};
-
-%% Receive Messages
 handle_info({publish, Topic, Payload}, State) ->
   io:format("Message from ~s: ~p~n", [Topic, Payload]),
   {noreply, State}.
 
-%subscribe
-subscribe(Pid, Topic, Qos) ->
-  io:format("In subscribe, Topic: ~p, QoS: ~p Pid: ~p ~n", [Topic, Qos, Pid]),
-  gen_server:call(?MODULE, {subscribe, Topic, Qos}).
+subscribe(Topic, Qos) ->
+  gen_server:cast(?MODULE, {subscribe, Topic, Qos}).
 
-%publish
-publish(Pid, Topic, Message, Qos) ->
-  io:format("In publish, Topic: ~p, Message: ~p QoS: ~p Pid: ~p ~n", [Topic, Message, Qos, Pid]),
-  gen_server:call(?MODULE, {publish, Topic, Message, Qos}).
+publish(Topic, Message, Qos) ->
+  gen_server:cast(?MODULE, {publish, Topic, Message, Qos}).
